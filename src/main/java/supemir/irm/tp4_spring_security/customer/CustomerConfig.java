91b65bf1abc8d4955b58fb076a09924176be53c5
Chebihi <chebihi.infinitysoft@google.com>
2022-03-30 12:14:42 +0000

    add roles & authorities for customers

--- src/main/java/supemir/irm/tp4_spring_security/customer/CustomerConfig.java
+++ src/main/java/supemir/irm/tp4_spring_security/customer/CustomerConfig.java
@@ -3,6 +3,7 @@ package supemir.irm.tp4_spring_security.customer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.webmvc.RestControllerConfiguration;
 
 @Configuration
 public class CustomerConfig {
@@ -16,7 +17,7 @@ public class CustomerConfig {
     @Bean
     public CommandLineRunner init(){
         return args -> {
-            for(int i=0;i<100;i++)
+            for(int i=1;i<=100;i++)
                 customerRepository.save(new Customer(null,"Client"+i,"client"+i+"@gmail.com"));
         };
     }
--- src/main/java/supemir/irm/tp4_spring_security/sec/ApplicationSecurityConfig.java
+++ src/main/java/supemir/irm/tp4_spring_security/sec/ApplicationSecurityConfig.java
@@ -1,26 +1,69 @@
 package supemir.irm.tp4_spring_security.sec;
 
+import org.hibernate.engine.internal.StatefulPersistenceContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
         http
+//                .authenticationManager()
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/","/index.html","/css/**","/js/**")
                 .permitAll()
+                .antMatchers(HttpMethod.GET,"/customers/**").hasAuthority("customer:read")
+                .antMatchers(HttpMethod.DELETE,"/customers/**").hasAuthority("customer:delete")
                 .and()
                 .authorizeRequests()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override @Bean
+    protected UserDetailsService userDetailsService() {
+
+        UserDetails user_admin = User
+                .builder()
+                .username("admin")
+                .password(passwordEncoder.encode("admin"))
+//                .roles("ADMIN")
+                .authorities("customer:read","customer:write","customer:delete")
+                .build();
+
+        UserDetails user_manager = User
+                .builder()
+                .username("manager")
+                .password(passwordEncoder.encode("1234"))
+                .authorities("customer:read")
+//                .roles("MANAGER")
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                user_admin,user_manager
+        );
 
     }
 }
